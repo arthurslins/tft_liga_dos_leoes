e01173973e19df19d9c520601fa408bfe71d496f
unknown <arthurslinss@gmail.com>
2022-04-07 15:26:57 -0300

    aaa

--- st_app_sections/forms.py
+++ st_app_sections/forms.py
@@ -28,8 +28,7 @@ def forms():
         st.write("")
     st.markdown("<h1 style='text-align: center; '>Liga dos Leões</h1>", unsafe_allow_html=True)
 
-    jogadores=pd.read_csv("jogadores.csv")
-    st.dataframe(jogadores)
+  
     
     
     
