7325f41c021fba28e4fd5ef8fb7f84a81f8d1802
unknown <arthurslinss@gmail.com>
2022-04-07 16:06:02 -0300

    aaa

--- tft_liga.py
+++ tft_liga.py
@@ -9,56 +9,56 @@ import base64
 st.set_page_config(layout="wide")
 base= "dark"
 
-# @st.cache(allow_output_mutation=True)
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
+# # @st.cache(allow_output_mutation=True)
+# # def get_base64_of_bin_file(bin_file):
+# #     with open(bin_file, 'rb') as f:
+# #         data = f.read()
+# #     return base64.b64encode(data).decode()
 
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file) 
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
+# # def set_png_as_page_bg(png_file):
+# #     bin_str = get_base64_of_bin_file(png_file) 
+# #     page_bg_img = '''
+# #     <style>
+# #     .stApp {
     
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     background-repeat: no-repeat;
-#     background-attachment: scroll; # doesn't work
+# #     background-image: url("data:image/png;base64,%s");
+# #     background-size: cover;
+# #     background-repeat: no-repeat;
+# #     background-attachment: scroll; # doesn't work
     
-#     }
-#     </style>
-#     ''' % bin_str
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
+# #     }
+# #     </style>
+# #     ''' % bin_str
+# #     st.markdown(page_bg_img, unsafe_allow_html=True)
     
-#     return
+# #     return
 
-# def set_png_as_page_bg2(png_file):
-#     bin_str = get_base64_of_bin_file(png_file) 
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
+# # def set_png_as_page_bg2(png_file):
+# #     bin_str = get_base64_of_bin_file(png_file) 
+# #     page_bg_img = '''
+# #     <style>
+# #     .stApp {
 
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     background-repeat: no-repeat;
-#     background-attachment: scroll; # doesn't work
+# #     background-image: url("data:image/png;base64,%s");
+# #     background-size: cover;
+# #     background-repeat: no-repeat;
+# #     background-attachment: scroll; # doesn't work
 
-#     }
-#     </style>
-#     ''' % bin_str
+# #     }
+# #     </style>
+# #     ''' % bin_str
     
-#     st.sidebar.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
+# #     st.sidebar.markdown(page_bg_img, unsafe_allow_html=True)
+# #     return
 
-# set_png_as_page_bg('the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp')
-# set_png_as_page_bg2('the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp')
+# # set_png_as_page_bg('the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp')
+# # set_png_as_page_bg2('the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp')
 
-main_bg = "the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp"
-main_bg_ext = "webp"
+# main_bg = "the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp"
+# main_bg_ext = "webp"
 
-side_bg = "the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp"
-side_bg_ext = "webp"
+# side_bg = "the-best-top-desktop-hd-dark-black-wallpapers-dark-black-wallpaper-dark-background-dark-wallpaper-23.webp"
+# side_bg_ext = "webp"
 
 st.markdown(
     f"""
